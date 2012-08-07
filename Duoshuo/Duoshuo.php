29ca5a0d1140b3f44631d94ecd1bfa41974c9020
root <root@tuchong-server6.(none)>
2012-08-08 02:10:48 +0800

    ignore Category namespace

--- Duoshuo/Duoshuo.php
+++ Duoshuo/Duoshuo.php
@@ -66,7 +66,7 @@ function onSkinAfterContent_AddDuoshuo(&$data, $skin)
 		|| !$wgTitle->canTalk()
 		|| $wgTitle->isTalkPage()
 		|| $wgTitle->isMainPage()
-		|| in_array($wgTitle->getNamespace(), array(NS_MEDIAWIKI, NS_TEMPLATE))
+		|| in_array($wgTitle->getNamespace(), array(NS_MEDIAWIKI, NS_TEMPLATE, NS_CATEGORY))
 		|| $wgOut->isPrintable()
 		|| $wgRequest->getVal('action', 'view') != "view")
 		return true;

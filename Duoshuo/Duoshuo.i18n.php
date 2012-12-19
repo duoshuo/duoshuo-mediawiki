2a5b3cbbf413e66d3b8dd392f626351139738f6e
zoglun <zoglun@gmail.com>
2012-12-18 21:42:27 -0600

    正确的繁简共存情况下的写法

--- Duoshuo/Duoshuo.i18n.php
+++ Duoshuo/Duoshuo.i18n.php
@@ -1,32 +1,29 @@
 <?php
 /**
  * Internationalisation file for Duoshuo extension.
- *
+ * 正确的多语言支持写法by ice@moegirl.org
  * @addtogroup Extensions
  */
  
 $messages = array(
 	'en' => array(
-        'duoshuo-desc'             => 'Integrates Duoshuo commenting service',
+        'duoshuo-desc'             => 'Duoshuo commenting extension',
         'duoshuo-before'           => '<h2>Share your opinion</h2>',
         'duoshuo-after'            => ''
 	),
-	'fi' => array(
-        'duoshuo-before'           => '<h2>Jaa mielipiteesi</h2>',
-        'duoshuo-after'            => ''
-	),
-	'de' => array(
-        'duoshuo-before'           => '<h2>Teile deine Meinung mit</h2>',
-        'duoshuo-after'            => ''
-	),
-	'es' => array(
-        'duoshuo-desc'             => 'Integra el servicio de comentarios Duoshuo',
-        'duoshuo-before'           => '<h2>Comparte tu opinión</h2>',
-        'duoshuo-after'            => ''
-	),
-	'zh-cn' => array(
-		'duoshuo-desc'             => '集成多说评论服务',
+	'zh' => array(
+		'duoshuo-desc'             => '多说评论插件',
 		'duoshuo-before'           => '<h2>你的看法</h2>',
+		'duoshuo-after'            => ''
+	),
+	'zh-hans' => array(
+		'duoshuo-desc'             => '多说评论插件',
+		'duoshuo-before'           => '<h2>留言</h2>',
+		'duoshuo-after'            => ''
+	),
+	'zh-hant' => array(
+		'duoshuo-desc'             => '多說評論插件',
+		'duoshuo-before'           => '<h2>留言</h2>',
 		'duoshuo-after'            => ''
 	)
 );
